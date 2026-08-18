d71aa257f3398ac4a5ae1ccdb695d5d6e97c9bac
Ian-loc <lara.clemente@ufla.br>
2026-08-17 21:19:32 -0300

    Fix CPTEC methodology URL for canonical HTTPS contract

--- .github/materializers/cptec_batch_v2.py
+++ .github/materializers/cptec_batch_v2.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 src=Path('.github/materializers/cptec_batch.py').read_text(encoding='utf-8')
+src=src.replace("'methodology_url':'http://bancodedados.cptec.inpe.br/'", "'methodology_url':'https://www.cptec.inpe.br/faq/como-acessar-os-dados-da-pagina-de-banco-de-dados-do-cptec/'")
 src=src.replace("'access_mechanism':", "'access_tool':")
 src=src.replace("'machine_readable':", "'free_download':")
 src=src.replace("'subset_support':", "'provider_attribution_required':")
