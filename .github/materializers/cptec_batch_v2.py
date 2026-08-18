a553a4d9360aac02d47348b2d6e6e4df49ca2eb9
Ian-loc <lara.clemente@ufla.br>
2026-08-17 21:20:18 -0300

    Use canonical HTTPS access documentation for CPTEC climatological database

--- .github/materializers/cptec_batch_v2.py
+++ .github/materializers/cptec_batch_v2.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
 src=Path('.github/materializers/cptec_batch.py').read_text(encoding='utf-8')
-src=src.replace("'methodology_url':'http://bancodedados.cptec.inpe.br/'", "'methodology_url':'https://www.cptec.inpe.br/faq/como-acessar-os-dados-da-pagina-de-banco-de-dados-do-cptec/'")
+faq="https://www.cptec.inpe.br/faq/como-acessar-os-dados-da-pagina-de-banco-de-dados-do-cptec/"
+src=src.replace("'methodology_url':'http://bancodedados.cptec.inpe.br/'", f"'methodology_url':'{faq}'")
+src=src.replace("'access_url':'http://bancodedados.cptec.inpe.br/'", f"'access_url':'{faq}'")
 src=src.replace("'access_mechanism':", "'access_tool':")
 src=src.replace("'machine_readable':", "'free_download':")
 src=src.replace("'subset_support':", "'provider_attribution_required':")
