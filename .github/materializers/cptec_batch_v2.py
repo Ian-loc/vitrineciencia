3b9809c67abf4b1156bd5b562c467276a91e5b9a
Ian-loc <lara.clemente@ufla.br>
2026-08-17 21:21:01 -0300

    Constrain CPTEC authentication metadata to canonical vocabulary

--- .github/materializers/cptec_batch_v2.py
+++ .github/materializers/cptec_batch_v2.py
@@ -4,6 +4,7 @@ src=Path('.github/materializers/cptec_batch.py').read_text(encoding='utf-8')
 faq="https://www.cptec.inpe.br/faq/como-acessar-os-dados-da-pagina-de-banco-de-dados-do-cptec/"
 src=src.replace("'methodology_url':'http://bancodedados.cptec.inpe.br/'", f"'methodology_url':'{faq}'")
 src=src.replace("'access_url':'http://bancodedados.cptec.inpe.br/'", f"'access_url':'{faq}'")
+src=src.replace("'authentication_required':'varia'", "'authentication_required':'desconhecido'")
 src=src.replace("'access_mechanism':", "'access_tool':")
 src=src.replace("'machine_readable':", "'free_download':")
 src=src.replace("'subset_support':", "'provider_attribution_required':")
